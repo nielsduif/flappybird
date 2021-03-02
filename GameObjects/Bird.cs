ac6914e810718bb16dbc5f26dae00d9a306944be
nielsduif <n.duivenn2@hva.nl>
2021-03-02 12:54:08 +0100

    push before lesson

---
+++ GameObjects/Bird.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlappyBird.GameObjects
+{
+    class Bird : SpriteGameObject
+    {
+        int fallAmount = 20;
+        int jumpAmount = 450;
+
+        public Bird() : base("spr_bird")
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+            velocity.Y = fallAmount;
+
+            position += velocity;
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            if (inputHelper.KeyPressed(Keys.Space))
+            {
+                velocity.Y -= jumpAmount;
+            }
+        }
+    }
+}
